romaonthego/REFrostedViewController

commit b4ea0573122ee46c4a2eec1b07f94db1366bbf09

only reset view if we want to animate

REFrostedViewController/REFrostedContainerViewController.m
@@ -96,24 +96,24 @@
     self.backgroundImageView.frame = self.view.bounds;
     self.frostedViewController.menuViewController.view.frame = self.containerView.bounds;
     
-    if (self.frostedViewController.direction == REFrostedViewControllerDirectionLeft) {
-        [self setContainerFrame:CGRectMake(- self.frostedViewController.calculatedMenuViewSize.width, 0, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height)];
-    }
-    
-    if (self.frostedViewController.direction == REFrostedViewControllerDirectionRight) {
-        [self setContainerFrame:CGRectMake(self.view.frame.size.width, 0, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height)];
-    }
-    
-    if (self.frostedViewController.direction == REFrostedViewControllerDirectionTop) {
-        [self setContainerFrame:CGRectMake(0, -self.frostedViewController.calculatedMenuViewSize.height, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height)];
-    }
-    
-    if (self.frostedViewController.direction == REFrostedViewControllerDirectionBottom) {
-        [self setContainerFrame:CGRectMake(0, self.view.frame.size.height, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height)];
-    }
-    
-    if (self.animateApperance)
+    if (self.animateApperance) {
+        if (self.frostedViewController.direction == REFrostedViewControllerDirectionLeft) {
+            [self setContainerFrame:CGRectMake(- self.frostedViewController.calculatedMenuViewSize.width, 0, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height)];
+        }
+        
+        if (self.frostedViewController.direction == REFrostedViewControllerDirectionRight) {
+            [self setContainerFrame:CGRectMake(self.view.frame.size.width, 0, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height)];
+        }
+        
+        if (self.frostedViewController.direction == REFrostedViewControllerDirectionTop) {
+            [self setContainerFrame:CGRectMake(0, -self.frostedViewController.calculatedMenuViewSize.height, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height)];
+        }
+        
+        if (self.frostedViewController.direction == REFrostedViewControllerDirectionBottom) {
+            [self setContainerFrame:CGRectMake(0, self.view.frame.size.height, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height)];
+        }
         [self show];
+    }
 }
 
 - (void)setContainerFrame:(CGRect)frame

commit 90243d92a6174b6b71d870a5659d7b7b7d223af6

fix initial animation appearance bug that was introduced with commit `b4ea0573122ee46c4a2eec1b07f94db1366bbf09`

REFrostedViewController/REFrostedContainerViewController.m
@@ -96,25 +96,26 @@
     self.backgroundImageView.frame = self.view.bounds;
     self.frostedViewController.menuViewController.view.frame = self.containerView.bounds;
     
+    CGRect frame = CGRectZero;
+    switch (self.frostedViewController.direction) {
+        case REFrostedViewControllerDirectionLeft:
+            frame = CGRectMake(- self.frostedViewController.calculatedMenuViewSize.width, 0, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height);
+            break;
+        case REFrostedViewControllerDirectionRight:
+            frame = CGRectMake(self.view.frame.size.width, 0, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height);
+            break;
+        case REFrostedViewControllerDirectionTop:
+            frame = CGRectMake(0, -self.frostedViewController.calculatedMenuViewSize.height, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height);
+            break;
+        case REFrostedViewControllerDirectionBottom:
+            frame = CGRectMake(0, self.view.frame.size.height, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height);
+            break;
+        default:
+            break;
+    }
+    [self setContainerFrame:frame];
+    
     if (self.animateApperance) {
-        CGRect frame = CGRectZero;
-        switch (self.frostedViewController.direction) {
-            case REFrostedViewControllerDirectionLeft:
-                frame = CGRectMake(- self.frostedViewController.calculatedMenuViewSize.width, 0, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height);
-                break;
-            case REFrostedViewControllerDirectionRight:
-                frame = CGRectMake(self.view.frame.size.width, 0, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height);
-                break;
-            case REFrostedViewControllerDirectionTop:
-                frame = CGRectMake(0, -self.frostedViewController.calculatedMenuViewSize.height, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height);
-                break;
-            case REFrostedViewControllerDirectionBottom:
-                frame = CGRectMake(0, self.view.frame.size.height, self.frostedViewController.calculatedMenuViewSize.width, self.frostedViewController.calculatedMenuViewSize.height);
-                break;
-            default:
-                break;
-        }
-        [self setContainerFrame:frame];
         [self show];
     }
 }